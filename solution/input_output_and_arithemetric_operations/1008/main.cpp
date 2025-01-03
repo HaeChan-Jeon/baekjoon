5d91bad02647e1493573f5acff75676ac8ba3405
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-01-03 16:49:24 +0900

    ADD: 1008 cpp solution

---
+++ solution/input_output_and_arithemetric_operations/1008/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+
+int main() {
+    double a, b;
+
+    std::cin >> a >> b;
+
+    std::cout << std::fixed << std::setprecision(9);
+    std::cout << a / b << std::endl;
+
+    return 0;
+}
