8ffee899d34c805057bfc4ba1c6ba60bf44b358e
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-01-04 21:22:48 +0900

    ADD: 11021 cpp solution

---
+++ solution/input_output_and_arithemetric_operations/11021/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int a;
+    std::cin >> a;
+
+    int b, c;
+
+    for (size_t i = 0; i < a; i++)
+    {
+        std::cin >> b >> c;
+        std::cout << "Case #" + std::to_string(i + 1) + ": " + std::to_string(b + c) << std::endl;
+    }
+
+    return 0;
+}
