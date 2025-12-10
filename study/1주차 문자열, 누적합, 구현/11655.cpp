ba6711e25de3957cf4f9688a3ccd869ecac8f334
JeonHaeChan <geojeonhc@gmail.com>
2025-12-11 08:48:36 +0900

    STUDY: 11655

---
+++ study/1주차 문자열, 누적합, 구현/11655.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string s;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	getline(cin, s);
+	
+	for(int i = 0 ; i < s.size(); ++i) {
+		if(s[i] >= 65 && s[i] <= 90) {
+			if(s[i] + 13 > 90) s[i] -= 26;
+			s[i] += 13;
+		}
+		if(s[i] >= 97 && s[i] <= 122) {
+			if(s[i] + 13 > 122) s[i] -= 26;
+			s[i] += 13;
+		}
+	}
+	
+	cout << s;
+	
+	return 0;
+}
