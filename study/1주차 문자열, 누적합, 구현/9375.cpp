1b98fd104fd5a5b41e9dbf19bf126223d608510c
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-12-16 09:21:29 +0900

    STUDY: 9375

---
+++ study/1주차 문자열, 누적합, 구현/9375.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, m, ret;
+string a, b;
+map<string, int> _map;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> n;
+	while(n--) {
+		cin >> m;
+		while(m--) {
+			cin >> a >> b;
+			_map[b]++;
+		}
+		ret = 1;
+		for(auto it : _map) {
+			ret *= it.second + 1;
+		}
+		ret -= 1;
+		cout << ret << "\n";
+		_map.clear();
+	}
+		
+	return 0;
+}
