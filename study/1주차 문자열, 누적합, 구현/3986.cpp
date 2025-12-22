2f5e9efd38648949a2bba5e72135ce6eabb390db
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-12-22 11:19:03 +0900

    STUDY: 3986

--- study/1주차 문자열, 누적합, 구현/3986.cpp
+++ study/1주차 문자열, 누적합, 구현/3986.cpp
@@ -5,25 +5,23 @@ int n, ret;
 string s;
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-	
 	cin >> n;
 	
 	while(n--) {
-		stack<char> stk;
 		cin >> s;
-		
+		stack<char> stk;	
 		stk.push(s[0]);
-		
+
 		for(int i = 1; i < s.size(); ++i) {
 			if(!stk.empty() && stk.top() == s[i]) {
-				stk.pop(); continue;
+				stk.pop();
+			} else {
+				stk.push(s[i]);
 			}
-			stk.push(s[i]);
 		}
-		
-		if(stk.empty()) ret++;
+		if(stk.empty()) {
+			ret++;
+		}
 	}
 	
 	cout << ret;
